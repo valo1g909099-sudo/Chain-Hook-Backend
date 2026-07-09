5fbf17be03a94c9840ab78b6fbc672c6dcce8436
valo1g909099-sudo <valo1g909099@gmail.com>
2026-07-09 10:38:57 +0600

    Update settings.py

--- Ewallet/settings.py
+++ Ewallet/settings.py
@@ -9,7 +9,7 @@ load_dotenv(BASE_DIR / '.env')
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
-DEBUG = os.environ.get('DEBUG', 'False') == 'True'
+DEBUG = False
 
 ALLOWED_HOSTS = [
     '127.0.0.1',
