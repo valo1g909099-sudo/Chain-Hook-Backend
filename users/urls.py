4914cb61aef6b3e10b0bbb68d6fc8c4d323e4eeb
valo1g909099-sudo <valo1g909099@gmail.com>
2026-07-10 14:00:34 +0600

    Update urls.py

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenRefreshView
-from .views import RegisterView, LoginView, ProfileView, LogoutView, ValidateClientView,GenerateClientTokenView, ValidateClientTokenView
-
+from .views import (
+    RegisterView,
+    LoginView,
+    ProfileView,
+    LogoutView,
+    GenerateClientTokenView,
+    ValidateClientTokenView,
+)
 
 urlpatterns = [
     path('auth/register/', RegisterView.as_view(), name='auth-register'),
@@ -9,8 +15,7 @@ urlpatterns = [
     path('auth/refresh/', TokenRefreshView.as_view(), name='auth-refresh'),
     path('auth/profile/', ProfileView.as_view(), name='auth-profile'),
     path('auth/logout/', LogoutView.as_view(), name='auth-logout'),
-    path('auth/validate-client/', ValidateClientView.as_view(), name='auth-validate-client'),
-     path('clients/generate-token/', GenerateClientTokenView.as_view(), name='generate-client-token'),
+
+    path('clients/generate-token/', GenerateClientTokenView.as_view(), name='generate-client-token'),
     path('clients/validate-token/', ValidateClientTokenView.as_view(), name='validate-client-token'),
 ]
-
